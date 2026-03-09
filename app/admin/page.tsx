cea24bb934188e630d041a92b51678ed8666fff8
Anshuman-sharma2006 <anshumansharma9451@gmail.com>
2026-03-09 22:23:49 +0530

    using Databse
    Here I Used Sqlite Too Setup My DataBase Locally

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,17 +1,31 @@
-import { Button } from "@/components/ui/button"
+// import { Button } from "@/components/ui/button"
+import { db } from "@/db/db"
+import { formatCurrency,formatNumber } from "@/lib/formatter"
 import {
   Card,
-  CardAction,
+  // CardAction,
   CardContent,
   CardDescription,
-  CardFooter,
+  // CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-export default function AdminDashboardPage() {
+const GetSalesData=async()=>{
+  const data=await db.order.aggregate({
+    _sum:{pricePaidIntRupees:true},
+    _count: {id: true }
+  })
+  return {
+  amount:(data._sum.pricePaidIntRupees||0)/100,
+  numberOfSales: data._count.id
+  }
+}
+
+export default async function AdminDashboardPage() {
+  const SalesData= await GetSalesData()
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      <DashboardCard title="Total Sales" subtitle="Last 30 days" body="'Rs.12,345" />
+      <DashboardCard title="Total Sales" subtitle={formatNumber(SalesData.numberOfSales)} body={formatCurrency(SalesData.amount)} />
       <DashboardCard title="Total Orders" subtitle="Last 30 days" body="Rs.123" />
       <DashboardCard title="Total Customers" subtitle="Last 30 days" body="Rs.45" />
 
--- db/db.ts
+++ db/db.ts
@@ -1,11 +1,15 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client" //* Here Its A Singeltone Structure So each request not Created new data base that make reduce from hot-reloading
 
 const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined
+  prisma: PrismaClient | undefined//* Save Database Globally 
 }
 
 export const db =
-  globalForPrisma.prisma ?? new PrismaClient()
+  globalForPrisma.prisma ?? new PrismaClient()//Use That globally Database Without Creating other That Make It SingeltonStr
 
 if (process.env.NODE_ENV !== "production")
   globalForPrisma.prisma = db
+
+
+
+ 
